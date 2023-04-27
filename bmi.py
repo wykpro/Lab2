8e5cfd3bd220e386b0a6b8df2ff6daf8cf67e25d
NigelWong <wyongkang.22@ichat.sp.edu.sg>
2023-04-27 16:53:47 +0800

    GG

--- bmi.py
+++ bmi.py
@@ -1,17 +1,16 @@
 def calculate_bmi(height, weight):
     print ("Height = " + str(height))
     print ("Weight = " + str(weight))
-weight=float(input("Weight ="))
-height=float(input("Height ="))
+    bmi = weight / height / height
+    print ("BMI = " + str(bmi))
 
-bmi = weight / height / height
-print ("BMI = " + str(bmi))
+    if bmi < 18.5 :
+        print("You Are Underweight")
+    if bmi > 25.0 :
+        print ("You Are Overweight")
+    else : print ("You Are Normal Weight")
 
-if bmi < 18.5 :
-    print("You Are Underweight")
-if bmi > 25.0 :
-    print ("You Are Overweight")
-else : print ("You Are Normal Weight")
+calculate_bmi(weight=57,height=1.73)
 
 
 
